21d6f2997f18b5b49a8b7c82c2be18ac60756f61
AnshikaPrajapati09 <anshikaprajapati0909@gmail.com>
2026-06-05 11:00:27 +0530

    Fix login issue

--- server/index.js
+++ server/index.js
@@ -11,8 +11,8 @@ import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
 app.use(cors({
-  origin: true,
-  credentials: true
+    origin:"https://three-interviewiq-1-gtsj.onrender.com",
+    credentials:true
 }))
 
 app.use(express.json())
